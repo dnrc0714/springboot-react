d1f42a5bbc3faa16fa6c3ede62c68dda7e0c02dd
dnrc0____0 <ziziang951@gmail.com>
2024-12-16 16:21:21 +0900

    inti project

---
+++ src/test/java/com/ccbb/demo/CcbbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccbb.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcbbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
